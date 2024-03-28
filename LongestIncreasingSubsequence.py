19ca27c7009eac7275623a40004847420504257b
jedi <jedi2454@gmail.com>
2024-03-28 18:27:44 -0400

    LeetCode 300. Longest Increasing Subsequence With DP BinarySearch

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from typing import List
 
 """_summary_
@@ -5,10 +6,13 @@ LeetCode 300. Longest Increasing Subsequence
 Using DP
 dp[1]*n. 
 every dp[i] have looking before dp[i] max seq
+
 #Leetcode150
-Time:O(n)
-Space:O(1)
+Time:O(n^2)
+Space:O(n)
 """
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1] * len(nums)
@@ -16,5 +20,18 @@ class Solution:
         for i in range(1, len(nums)):
             for j in range(0, i):
                 if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1) # cur index is i, j is looking for big seq nums.
+                    dp[i] = max(
+                        dp[i], dp[j] + 1
+                    )  # cur index is i, j is looking for big seq nums.
         return max(dp)
+
+    def lengthOfLIS2(self, nums: List[int]) -> int:
+        dp = []
+
+        for i in nums:
+            idx = bisect_left(dp, i)
+            if idx == len(dp):
+                dp.append(i)
+            else:
+                dp[idx] = i
+        return len(dp)
